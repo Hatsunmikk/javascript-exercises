ba736c2cb5a0284eda84ddf5af5468b7af074561
Hatsunmikk <debos2003@gmail.com>
2024-12-04 14:50:45 +0530

    First modification

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+/*const repeatString = function() {
 
+};  */
+
+let wordToRepeat = "hey";
+let repetitions = 3;
+
+function repeatString(word, number){
+    for(let i = 0; i < number; i++){
+        return word;
+    };
 };
+
+repeatString(wordToRepeat, repetitions);
 
 // Do not edit below this line
 module.exports = repeatString;
