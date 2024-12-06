265554e7372fb6712b141e9fe6ec8329698b1270
Hatsunmikk <debos2003@gmail.com>
2024-12-06 13:39:16 +0530

    Rectify mistakes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
 const sumAll = function(start, end) {
 
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+
+    if(start < 0 || end < 0) return "ERROR";
+
+    if(start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    };
+
     let sum = 0;
 
     for(let i = start; i <= end; i++){
